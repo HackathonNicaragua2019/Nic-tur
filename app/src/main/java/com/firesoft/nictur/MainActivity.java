43052d5809c2ba6971c70e31ed30c0030b848874
Xavybarcenas <barcenasbyron99@gmail.com>
2019-11-09 12:09:52 -0600

    version 1

--- app/src/main/java/com/firesoft/nictur/MainActivity.java
+++ app/src/main/java/com/firesoft/nictur/MainActivity.java
@@ -2,7 +2,9 @@ package com.firesoft.nictur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void depa_leon(View view) {
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
